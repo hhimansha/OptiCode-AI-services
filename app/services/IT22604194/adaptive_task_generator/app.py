eff132aaf18747cda3e27c62724385bc42c1145e
G.V.G.A.S. Hettiarachchi <IT22604194@my.sliit.lk>
2026-01-07 20:22:34 +0530

    changed adaptive_task_generator

--- app/services/IT22604194/adaptive_task_generator/app.py
+++ app/services/IT22604194/adaptive_task_generator/app.py
@@ -1,10 +1,11 @@
-# adaptive_task_generator/app.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from utils import load_dataset
-from generator import pick_reference_problem, build_prompt, generate_task
+from generator import pick_reference_problem, build_prompt
+from qwen_client import generate_with_qwen
+from gemini_client import generate_with_fallback
 
 app = FastAPI(title="Adaptive Coding Task Generator")
 
@@ -23,24 +24,36 @@ app.add_middleware(
 # Load dataset once at startup
 dataset = load_dataset("new_dataset.jsonl")
 
-
 class SkillRequest(BaseModel):
     student_skill: int  # 1–5
-
 
 @app.post("/generate-task")
 def generate_task_api(body: SkillRequest):
     example = pick_reference_problem(dataset, body.student_skill)
     prompt = build_prompt(example, body.student_skill)
-    task = generate_task(prompt)
+
+    task = ""
+    used_model = "qwen"
+
+    try:
+        task, qwen_time = generate_with_qwen(body.student_skill)
+
+
+        # fallback conditions
+        if qwen_time > 15 or len(task) < 20:
+            task = generate_with_fallback(prompt)
+            used_model = "gemini"
+
+    except Exception:
+        task = generate_with_fallback(prompt)
+        used_model = "gemini"
 
     return {
         "skill_level": body.student_skill,
         "reference_concept": example.get("concept"),
-        "reference_task": example.get("task"),
         "generated_task": task,
+        "generated_by": used_model
     }
-
 
 @app.get("/")
 def root():
